635765fabef0dc1ed2a746a25226231108bf7b5c
Jason Garber <jason@sixtwothree.org>
2025-01-09 15:56:48 -0500

    Add actor JSON URL to aliases

--- src/.well-known/webfinger.11ty.js
+++ src/.well-known/webfinger.11ty.js
@@ -8,7 +8,7 @@ export default class {
 
   render({ app }) {
     const subject = "acct:refreshdc@refresh-dc.org";
-    const aliases = [app.start_url];
+    const aliases = [app.start_url, new URL("activitypub/actor.json", app.start_url)];
     const links = [
       {
         href: new URL(app.icons[0].src, app.start_url),
@@ -27,6 +27,7 @@ export default class {
       },
     ];
 
-    return JSON.stringify({ aliases, links, subject }, null, 2);
+    /* eslint-disable-next-line sort-keys */
+    return JSON.stringify({ subject, aliases, links }, null, 2);
   }
 }
