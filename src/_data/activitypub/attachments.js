1eb74009af19c2a8210a062a72bfea5f85c8b6de
Jason Garber <jason@sixtwothree.org>
2025-01-13 23:30:57 -0500

    Add ActivityPub and Webfinger data files

---
+++ src/_data/activitypub/attachments.js
@@ -0,0 +1,21 @@
+import actor from "./actor.js";
+
+const { url } = actor;
+
+export default [
+  {
+    name: "Website",
+    type: "PropertyValue",
+    value: `<a href="${url}" rel="me"><span class="invisible">https://</span>${url.hostname}</a>`,
+  },
+  {
+    name: "Newsletter",
+    type: "PropertyValue",
+    value: `<a href="${url}/newsletter" rel="me"><span class="invisible">https://</span>${url.hostname}/newsletter</a>`,
+  },
+  {
+    name: "LinkedIn",
+    type: "PropertyValue",
+    value: `<a href="https://www.linkedin.com/company/refresh-dc/" rel="me"><span class="invisible">https://</span>www.linkedin.com/company/refresh-dc/</a>`,
+  },
+]
