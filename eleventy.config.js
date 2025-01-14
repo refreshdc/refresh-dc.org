e702504abe8e09d59e79f0d5cbbb00d3323728d8
Jason Garber <jason@sixtwothree.org>
2025-01-13 23:29:55 -0500

    Use eleventy-plugin-webfinger

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 import eleventyPluginLiquid from "@jgarber/eleventy-plugin-liquid";
 import eleventyPluginMarkdown from "@jgarber/eleventy-plugin-markdown";
 import eleventyPluginRoutes from "@jgarber/eleventy-plugin-routes";
+import eleventyPluginWebfinger from "@jgarber/eleventy-plugin-webfinger";
 
 import markdownItAttrs from "markdown-it-attrs";
 
@@ -25,6 +26,7 @@ export default function(eleventyConfig) {
   // Plugins
   eleventyConfig.addPlugin(eleventyPluginLiquid);
   eleventyConfig.addPlugin(eleventyPluginRoutes);
+  eleventyConfig.addPlugin(eleventyPluginWebfinger);
 
   eleventyConfig.addPlugin(eleventyPluginMarkdown, {
     plugins: [markdownItAttrs],
